62e5dda9522c524f8013f6dd5cc1c091da6444a4
Vinay DG <vinaydg2001@gmail.com>
2025-01-26 11:53:15 +0530

    add function to get the human player choice

--- game.js
+++ game.js
@@ -1,3 +1,23 @@
 function getComputersChoice() {
   return Math.floor(Math.random() * 3) + 1;
 }
+
+function getHumanChoice() {
+  let choice = prompt("Give you choice. Rock, Paper or Scissor?").toLowerCase();
+  let choiceNumber = undefined;
+  switch (choice) {
+    case "rock":
+      choiceNumber = 1;
+      break;
+    case "paper":
+      choiceNumber = 2;
+      break;
+    case "scissor":
+      choiceNumber = 3;
+      break;
+
+    default:
+      getHumanChoice();
+  }
+  return choiceNumber;
+}
